6b1102936bb704b9294067ad7fc330b20706a97f
skagenmacka <54712382+skagenmacka@users.noreply.github.com>
2021-06-30 12:20:43 +0000

    Initial commit

---
+++ localhost.bat
@@ -0,0 +1,2 @@
+start "" "http://localhost:8000/"
+py -3 -m http_server.py
---
+++ src/app.js
@@ -0,0 +1,40 @@
+/* Sätter tilbaka top-bar till toppen eftersom JavaScipt fungerar */
+const topBra = document.getElementById("top-bar");
+topBra.style.marginTop = 0;
+topBra.style.top = 0;
+
+const langSelector = document.getElementById("lang-selector");
+const langDisplay = document.getElementById("lang-display");
+const langContent = document.getElementById("lang-content");
+
+const langs = 
+[
+    "en",
+    "sv",
+    "es",
+    "de",
+    "pr"
+]
+
+/* Skapar en lista utav alla språken */
+for(let i = 0; i < langs.length; i++)
+{
+    let node = document.createElement("img");
+    node.src = "res/flags/" + langs[i] + ".png";
+    
+    node.addEventListener("click", (e) => 
+    {
+        langDisplay.src = "res/flags/" + langs[i] + ".png";
+    });
+    
+    langContent.appendChild(node);
+}
+
+let langVisibility = false;
+
+langSelector.addEventListener("click", (e) =>
+{
+    langVisibility = !langVisibility;
+    langContent.style.opacity = langVisibility == true ? 1 : 0;
+    langContent.style.pointerEvents = langVisibility == true ? "all" : "none";
+});
