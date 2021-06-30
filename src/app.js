1cbb72bbb6d5c0945fb41b16dbed1394f5f2b3f0
skagenmacka <54712382+skagenmacka@users.noreply.github.com>
2021-06-30 14:11:15 +0000

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 const topBra = document.getElementById("top-bar");
 topBra.style.marginTop = 0;
 
+/* Språk väljar grejer */
 const langSelector = document.getElementById("lang-selector");
 const langDisplay = document.getElementById("lang-display");
 const langContent = document.getElementById("lang-content");
@@ -34,6 +35,23 @@ let langVisibility = false;
 langSelector.addEventListener("click", (e) =>
 {
     langVisibility = !langVisibility;
-    langContent.style.opacity = langVisibility == true ? 1 : 0;
-    langContent.style.pointerEvents = langVisibility == true ? "all" : "none";
+    langContent.style.opacity = langVisibility === true ? 1 : 0;
+    langContent.style.pointerEvents = langVisibility === true ? "all" : "none";
 });
+
+/* Naviagations panelen för mobilen och mindre skärmar */
+const mobileNavToggle = document.getElementById("mobile-nav-toggle");
+const mobileNavBar = document.getElementById("mobile-nav-bar");
+let navBarVisisble = false;
+
+mobileNavToggle.addEventListener("click", (e) =>
+{
+    navBarVisisble = !navBarVisisble;
+    mobileNavBar.style.transform = navBarVisisble === true ? "translateX(100%)" : "translateX(0%)";
+});
+
+
+
+
+
+
