8b558bd0fc2fdc02e4a9f983e815afa106bcfb3a
skagenmacka <54712382+skagenmacka@users.noreply.github.com>
2021-06-30 17:19:07 +0000

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -7,10 +7,16 @@ const mobileNavToggle = document.getElementById("mobile-nav-toggle");
 const mobileNavBar = document.getElementById("mobile-nav-bar");
 let navBarVisisble = false;
 
+const mobileBurger = document.getElementById("mobile-burger");
+const mobileX = document.getElementById("mobile-x");
+
 mobileNavToggle.addEventListener("click", (e) =>
 {
     navBarVisisble = !navBarVisisble;
     mobileNavBar.style.transform = navBarVisisble === true ? "translateX(100%)" : "translateX(0%)";
+    
+    mobileBurger.style.opacity = navBarVisisble === true ? 0 : 1;
+    mobileX.style.opacity = navBarVisisble === true ? 1 : 0;
 });
 
 
