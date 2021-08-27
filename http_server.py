0c18cb23cd73b2c2bb2b38cb6a97af03817fb710
skagenmacka <54712382+skagenmacka@users.noreply.github.com>
2021-08-27 20:23:37 +0200

    Add files via upload

--- http_server.py
+++ http_server.py
@@ -11,9 +11,9 @@ PORT = 8000
 Handler = http.server.SimpleHTTPRequestHandler
 
 Handler.extensions_map={
-    '.manifest': 'text/cache-manifest',
+        '.manifest': 'text/cache-manifest',
 	'.html': 'text/html',
-    '.png': 'image/png',
+        '.png': 'image/png',
 	'.jpg': 'image/jpg',
 	'.svg':	'image/svg+xml',
 	'.css':	'text/css',
--- localhost.bat
+++ localhost.bat
@@ -1,2 +1,4 @@
 start "" "http://localhost:8000/"
+
+REM py -3 -m http.server
 py -3 -m http_server.py
